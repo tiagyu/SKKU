c3bdc58e355aa36a93e609fbf465199a9c60b6ba
tiagyu <tkroh1997@gmail.com>
2025-03-22 20:24:05 +0900

    Update lab02_ex05.py

--- Immersive_Media_Programming/Week3/lab02_ex05.py
+++ Immersive_Media_Programming/Week3/lab02_ex05.py
@@ -6,12 +6,14 @@ import cv2 as cv
 img = np.zeros((512,512,3), np.uint8)
  
 # Draw a diagonal blue line with thickness of 5 px
+# 좌표 (0,0) -> (511,511) 색상 (255,0,0) 굵기 5
 cv.line(img,(0,0),(511,511),(255,0,0),5)
 
 # Drawing Rectangle
 cv.rectangle(img,(384,0),(510,128),(0,255,0),3)
 
 # Drawing Circle
+# 중심 (447,63) 반지름 63 채운 그림 -1
 cv.circle(img,(447,63), 63, (0,0,255), -1)
 
 # Drawing Ellipse
